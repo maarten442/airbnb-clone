719d77dedcddf88129e919fe5d47aa8c19bd1d78
maarten.rottier <maarten.rottier@postcodeloterij.nl>
2023-12-30 10:29:45 +0100

    added small changes to navbar

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -57,7 +57,7 @@ const UserMenu = () => {
                         rounded-full
                         cursor-pointer
                         hover:shadow-md
-                        transtion
+                        transition
                     "
                 >
                     < AiOutlineMenu />
